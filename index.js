8134ad942334e7223a74c952f6fdd660043d7752
shalom-nganga <minnomacharia@gmail.com>
2023-10-09 08:39:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(string){
+string = typeof string !=="undefined" ? 'bathe my dog' : 'roller-skate';
+    return `This saturday,i want to ${string}!`
+}
+const mondayWork= function(string){
+    string=typeof string !=="undefined"? 'work from home': 'go to the office';
+    return `this Monday.i will ${string}.`
+}
+function wrapAdjective(string){
+    return function(){
+        if (string=== "||"){
+            return `you are ${string}a dedicated programmer ${string}!`}
+            else return `you are ${string}a hard worker ${string}!`   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
